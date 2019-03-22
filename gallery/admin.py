e4ce1f7b695e9a6adb4bf186f878ade7a9f5f4c8
wangpeng1234 <wangpeng1234321@126.com>
2019-03-22 14:14:56 +0800

    修改了标题

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Gallery # 从当前文件路径中的models中导入app（不要忘记迁移命令）
+from .models import Gallery # 从当前文件路径中的models中导入app
+#（不要忘记迁移命令，访问需创建超级管理员权限）
 
 # Register your models here.
 
